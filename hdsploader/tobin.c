d8fa011494a7c1be13ef29a083b0addbebf9548d
Jaroslav Kysela <perex@perex.cz>
2003-11-25 15:36:00 +0000

    Ok, things are working now...

--- hdsploader/tobin.c
+++ hdsploader/tobin.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <endian.h>
 
 #include "digiface_firmware.dat"
 #include "digiface_firmware_rev11.dat"
@@ -13,6 +14,16 @@ int write_bin_file(u_int32_t *array, const char *filename)
 	
 	if ((out = fopen(filename, "w+")) == NULL)
 		return -1;
+#if __BYTE_ORDER == __BIG_ENDIAN
+	{
+		unsigned int idx;
+		for (idx = 0; idx < 24413; idx++)
+			array[idx] = ((array[idx] & 0x000000ff) << 16) |
+				     ((array[idx] & 0x0000ff00) << 8)  |
+				     ((array[idx] & 0x00ff0000) >> 8)  |
+				     ((array[idx] & 0xff000000) >> 16);
+	}
+#endif
 	if (fwrite(array, 4, 24413, out) != 24413) {
 		fclose(out);
 		return -1;
