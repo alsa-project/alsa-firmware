d46140583fd3c98748dfdaa6344f79e13e729da4
Clemens Ladisch <clemens@ladisch.de>
2006-11-06 09:20:56 +0100

    wavefront: simplify YSS225 register initialization
    Instead of using a somewhat algorithmic approach of initializing the
    YSS225's registers, just use a simple series of port/value pairs.
    This makes it easier to later replace or entirely remove the register
    data blob from the driver.

--- wavefront/fw_writer.c
+++ wavefront/fw_writer.c
@@ -20,76 +20,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
+
+#define WAIT_IDLE 0xff
 
 #include "yss225.c"
 
-#define SIZE_PAGE_ZERO		(128 * 2)
-#define SIZE_PAGE_ONE		(128 * 2)
-#define SIZE_PAGE_TWO		(128 * 1)
-#define SIZE_PAGE_THREE		(128 * 1)
-#define SIZE_PAGE_FOUR		(128 * 1)
-#define SIZE_PAGE_SIX		(64 * 3)
-#define SIZE_PAGE_SEVEN		(128 * 2)
-#define SIZE_PAGE_ZERO_V2	(48 * 2)
-#define SIZE_PAGE_ONE_V2	(48 * 2)
-#define SIZE_PAGE_TWO_V2	(48 * 1)
-#define SIZE_PAGE_THREE_V2	(48 * 1)
-#define SIZE_PAGE_FOUR_V2	(48 * 1)
-#define SIZE_PAGE_SEVEN_V2	(48 * 2)
-#define SIZE_MOD_V2		(152 * 2)
-#define SIZE_COEFFICIENTS	(91 * 4)
-#define SIZE_COEFFICIENTS2	(14 * 4)
-#define SIZE_COEFFICIENTS3	(202 * 2)
-#define SIZE_REGISTER_DUMP	2904
-
 #define FILENAME "yss225_registers.bin"
-
-static FILE *f;
-static size_t written = 0;
-
-static void write_array(const unsigned char data[], size_t size)
-{
-	if (fwrite(data, 1, size, f) != size) {
-		perror("cannot write");
-		exit(EXIT_FAILURE);
-	}
-	written += size;
-}
 
 int main(void)
 {
+	FILE *f;
+
 	fprintf(stderr, "writing %s ...\n", FILENAME);
 	f = fopen(FILENAME, "wb");
 	if (!f) {
 		perror("cannot create file");
 		exit(EXIT_FAILURE);
 	}
-
-#define WRITE(data, size) do { \
-				assert(sizeof data == size); \
-				write_array(data, size); \
-			} while (0)
-	WRITE(page_zero, SIZE_PAGE_ZERO);
-	WRITE(page_one, SIZE_PAGE_ONE);
-	WRITE(page_two, SIZE_PAGE_TWO);
-	WRITE(page_three, SIZE_PAGE_THREE);
-	WRITE(page_four, SIZE_PAGE_FOUR);
-	WRITE(page_six, SIZE_PAGE_SIX);
-	WRITE(page_seven, SIZE_PAGE_SEVEN);
-	WRITE(page_zero_v2, SIZE_PAGE_ZERO_V2);
-	WRITE(page_one_v2, SIZE_PAGE_ONE_V2);
-	WRITE(page_two_v2, SIZE_PAGE_TWO_V2);
-	WRITE(page_three_v2, SIZE_PAGE_THREE_V2);
-	WRITE(page_four_v2, SIZE_PAGE_FOUR_V2);
-	WRITE(page_seven_v2, SIZE_PAGE_SEVEN_V2);
-	WRITE(mod_v2, SIZE_MOD_V2);
-	WRITE(coefficients, SIZE_COEFFICIENTS);
-	WRITE(coefficients2, SIZE_COEFFICIENTS2);
-	WRITE(coefficients3, SIZE_COEFFICIENTS3);
-	assert(written == SIZE_REGISTER_DUMP);
-
-	if (ferror(f) || fclose(f) == EOF) {
+	if (fwrite(data, 1, sizeof data, f) != sizeof data ||
+	    fclose(f) == EOF) {
 		perror("cannot write");
 		exit(EXIT_FAILURE);
 	}
